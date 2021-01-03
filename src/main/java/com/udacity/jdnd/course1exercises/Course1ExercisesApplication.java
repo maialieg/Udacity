2dec78bc4879bd9b4dc8cebe72339f78d3e0f2fc
Mai <maialieg@gmail.com>
2021-01-03 20:48:52 +0100

    First Excersice, initial commit

---
+++ src/main/java/com/udacity/jdnd/course1exercises/Course1ExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.udacity.jdnd.course1exercises;
+
+import org.springframework.boot.SpringApplication;
+
+@springbootapplication
+public class Course1ExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1ExercisesApplication.class, args);
+	}
+
+}
